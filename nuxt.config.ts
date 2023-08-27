f9ecdf45d048776e271dd2e4177c23db83bb4052
Flamberg <flamberg@list.ru>
2023-08-27 18:22:11 +0700

    baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,20 +4,15 @@ import svgLoader from "vite-svg-loader";
 import * as messages from "./locales";
 
 import appConfig from "./app-config";
-const {
-  BASE_URL,
-  API_KEY,
-  LOCALE,
-  LOCALES,
-  CITIES_URL,
-  CITIES_KEY,
-  IP_URL
-} = appConfig;
+const { BASE_URL, API_KEY, LOCALE, LOCALES, CITIES_URL, CITIES_KEY, IP_URL } =
+  appConfig;
 
 export default defineNuxtConfig({
   telemetry: false,
   ssr: false,
   app: {
+    baseURL: "/weather/",
+    buildAssetsDir: "assets",
     pageTransition: false,
     layoutTransition: false,
     head: {
@@ -50,7 +45,7 @@ export default defineNuxtConfig({
       api_key: API_KEY,
       cities_url: CITIES_URL,
       cities_key: CITIES_KEY,
-      ip_url: IP_URL
+      ip_url: IP_URL,
     },
   },
 
